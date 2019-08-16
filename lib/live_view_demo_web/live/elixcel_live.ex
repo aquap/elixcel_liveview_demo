32c30180c905dfb037afa1380fb5574e7378b4e0
Jørgen Orehøj Erichsen <joe@erichsen.net>
2019-08-16 20:22:20 +0200

    Cell navigation sort of works

--- lib/live_view_demo_web/live/elixcel_live.ex
+++ lib/live_view_demo_web/live/elixcel_live.ex
@@ -1,34 +1,64 @@
 defmodule LiveViewDemoWeb.ElixcelLive do
   use Phoenix.LiveView
-  import Calendar.Strftime
 
   def render(assigns) do
     ~L"""
-    <table>
+    <table phx-keydown="keydown" phx-target="window">
       <tbody>
         <tr><td></td><td>A</td><td>B</td></tr>
-        <tr><td>1</td><td></td><td></td></tr>
-        <tr><td>2</td><td></td><td></td></tr>
+        <%= for {row, row_index} <- rows(@sheet) do %>
+          <tr>
+            <td><%= row_index + 1 %></td>
+            <%= for {cell, column_index} <- cells(row) do %>
+              <td <%= active?(column_index, row_index, @current_cell) %>><%= cell %></td>
+            <% end %>
+           </tr>
+        <% end %>
       </tbody>
     </table>
 
     <style>
       td { border: 1px solid #e1e1e1; }
+      td.active { background-color: #dff4fb; }
     </style>
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-
-    {:ok, put_date(socket)}
+    {:ok, assign(socket, sheet: [[nil, nil, nil], [nil, nil, nil]], current_cell: [0, 0])}
   end
 
-  def handle_info(:tick, socket) do
-    {:noreply, put_date(socket)}
+  def handle_event("keydown", "ArrowRight", socket) do
+    [current_column, current_row] = socket.assigns.current_cell
+    {:noreply, assign(socket, current_cell: [current_column + 1, current_row])}
   end
 
-  defp put_date(socket) do
-    assign(socket, date: :calendar.local_time())
+  def handle_event("keydown", "ArrowLeft", socket) do
+    [current_column, current_row] = socket.assigns.current_cell
+    {:noreply, assign(socket, current_cell: [current_column - 1, current_row])}
   end
+
+  def handle_event("keydown", "ArrowUp", socket) do
+    [current_column, current_row] = socket.assigns.current_cell
+    {:noreply, assign(socket, current_cell: [current_column, current_row - 1])}
+  end
+
+  def handle_event("keydown", "ArrowDown", socket) do
+    [current_column, current_row] = socket.assigns.current_cell
+    {:noreply, assign(socket, current_cell: [current_column, current_row + 1])}
+  end
+
+  defp rows(_sheet) do
+    Enum.with_index([["x", "x2"], ["y", "y2"]])
+  end
+
+  defp cells(row) do
+    Enum.with_index(row)
+  end
+
+  defp active?(column, row, current_cell) do
+    [current_column, current_row] = current_cell
+    column == current_column && row == current_row && "class=active" || ""
+  end
+
 end
