c398cd2f1c21492d0738b7d6cb7187927c9bb333
Jørgen Orehøj Erichsen <joe@erichsen.net>
2019-09-27 20:49:27 +0200

    Put demo data in a separate module

--- lib/live_view_demo_web/live/elixcel_live.ex
+++ lib/live_view_demo_web/live/elixcel_live.ex
@@ -18,25 +18,6 @@ defmodule LiveViewDemoWeb.ElixcelLive do
   @meta_i %{"key" => "i", "metaKey" => true}
   @ctrl_i %{"key" => "i", "ctrlKey" => true}
 
-  @initial_cells %{
-    [2, 1] => %{value: "Quantity", format: %{bold: true}},
-    [3, 1] => %{value: "Price", format: %{bold: true}},
-    [4, 1] => %{value: "Total", format: %{bold: true}},
-    [1, 2] => %{value: "Apples"},
-    [2, 2] => %{value: "4"},
-    [3, 2] => %{value: "2"},
-    [4, 2] => %{value: "= B2 * C2"},
-    [1, 3] => %{value: "Bananas"},
-    [2, 3] => %{value: "5"},
-    [3, 3] => %{value: "3"},
-    [4, 3] => %{value: "= B3 * C3"},
-    [1, 4] => %{value: "Pears"},
-    [2, 4] => %{value: "6"},
-    [3, 4] => %{value: "5"},
-    [4, 4] => %{value: "= B4 * C4"},
-    [4, 5] => %{value: "D2 + D3 + D4", format: %{bold: true}}
-  }
-
   def render(assigns) do
     ~L"""
 
@@ -107,7 +88,7 @@ defmodule LiveViewDemoWeb.ElixcelLive do
 
   def mount(_session, socket) do
     {:ok,
-     assign(socket, rows: 6, cols: 6, current_cell: [1, 1], cells: @initial_cells, editing: false)}
+     assign(socket, rows: 6, cols: 6, current_cell: [1, 1], cells: ElixcelLive.DemoData.cells(), editing: false)}
   end
 
   # Keyboard events
---
+++ lib/live_view_demo_web/live/elixcel_live/demo_data.ex
@@ -0,0 +1,26 @@
+defmodule ElixcelLive.DemoData do
+
+  @initial_cells %{
+    [2, 1] => %{value: "Quantity", format: %{bold: true}},
+    [3, 1] => %{value: "Price", format: %{bold: true}},
+    [4, 1] => %{value: "Total", format: %{bold: true}},
+    [1, 2] => %{value: "Apples"},
+    [2, 2] => %{value: "4"},
+    [3, 2] => %{value: "2"},
+    [4, 2] => %{value: "= B2 * C2"},
+    [1, 3] => %{value: "Bananas"},
+    [2, 3] => %{value: "5"},
+    [3, 3] => %{value: "3"},
+    [4, 3] => %{value: "= B3 * C3"},
+    [1, 4] => %{value: "Pears"},
+    [2, 4] => %{value: "6"},
+    [3, 4] => %{value: "5"},
+    [4, 4] => %{value: "= B4 * C4"},
+    [4, 5] => %{value: "D2 + D3 + D4", format: %{bold: true}}
+  }
+
+  def cells() do
+    @initial_cells
+  end
+
+end
