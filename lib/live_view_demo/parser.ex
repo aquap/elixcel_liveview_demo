88b8be928f86136ad352b5867a43045ca1c4f8ab
Jørgen Orehøj Erichsen <joe@erichsen.net>
2019-09-27 07:45:23 +0200

    This file is not used - we use Abacus instead for now

--- lib/live_view_demo/parser.ex
+++
@@ -1,9 +0,0 @@
-defmodule Parser do
-  # import NimbleParsec
-
-  # digits = ascii_string([?0..?9], min: 1)
-  # number_integer = digits
-  # number_float = digits |> string(".") |> concat(digits)
-
-  # defparsec(:expression, number_float)
-end
