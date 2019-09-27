1c73dd6ac6b5821c8de5d3f50b53c7ebcdaf96ed
Jørgen Orehøj Erichsen <joe@erichsen.net>
2019-09-27 07:43:34 +0200

    Run the Prettier formatter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,30 +9,36 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
 
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
-import { Socket } from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import { Socket } from "phoenix";
+import LiveSocket from "phoenix_live_view";
 
-let Hooks = {}
+let Hooks = {};
 
 Hooks.SetFocus = {
-    mounted() {
-      this.el.focus();
-      this.el.selectionStart = this.el.selectionEnd = this.el.value.length;
-    }
-}
+  mounted() {
+    this.el.focus();
+    this.el.selectionStart = this.el.selectionEnd = this.el.value.length;
+  }
+};
 
 document.addEventListener("keydown", event => {
-  if (event.key == 'ArrowUp' || event.key == 'ArrowDown' || event.key == 'ArrowLeft' || event.key == 'ArrowRight' || (event.metaKey && event.key == 'b')) {
+  if (
+    event.key == "ArrowUp" ||
+    event.key == "ArrowDown" ||
+    event.key == "ArrowLeft" ||
+    event.key == "ArrowRight" ||
+    (event.metaKey && event.key == "b")
+  ) {
     event.preventDefault();
-  } 
+  }
 });
 
-let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks })
-liveSocket.connect()
+let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks });
+liveSocket.connect();
